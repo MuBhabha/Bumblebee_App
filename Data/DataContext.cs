08436362f58c4dbed95ef997ed56b0b37b73fd8b
ST10084009 <muhammedumairbhabha89@gmail.com>
2024-11-20 23:35:48 +0200

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using BumbleBee.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BumbleBee.Data
+{
+	public class DataContext:IdentityDbContext
+	{
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
